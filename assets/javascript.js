7a684563dcffbc02c4492ad418332d11706dae76
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-28 18:24:28 -0500

    testing js

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,59 +1,72 @@
+//variables
+var giphyKey = 'qde5re80EUg2L5yAKth9QabSkIrGiKWb'
+var weatherKey ='324a506b2f6b0f1b44fde14916e4b006'
 
-function getImage(keyword){
-    //get keyword valie
-    var keyword = $("#input").val().trim()
+
+var location =  $("gif-container")
+
+
+console.log('testing javascript');
+
+// var keyword = $("#input").val().trim()
+// function getImage(keyword){
+//     //get keyword valie
     
-    //get a G giphy 
-    fetch("https://api.giphy.com/v1/gifs/search?api_key="+ giphyKey +"&q="+ keyword +"&limit=25&offset=0&rating=g&lang=en")
-    .then(function(response){
-        console.log(response)
-       return response.json(); 
-    })
-    .then(function(data){
-        console.log(data)
+//     //get a G giphy 
+//     fetch("https://api.giphy.com/v1/gifs/search?api_key="+ giphyKey +"&q="+ keyword +"&limit=25&offset=0&rating=g&lang=en")
+//     .then(function(response){
+//         console.log(response)
+//        return response.json(); 
+//     })
+//     .then(function(data){
+//         console.log(data)
 
-        //empty html container
-        $('#gif-container').empty()
+//         //empty html container
+//         $('#gif-container').empty()
         
-        //find the link for the gif's
-        var imageUrl = data[0].images.original.url
-        //append image
-        $('#gif-container').append("<img id='gif' src=" + imageUrl + ">")
+//         //find the link for the gif's
+//         var imageUrl = data[0].images.original.url
+//         //append image
+//         $('#gif-container').append("<img id='gif' src=" + imageUrl + ">")
+//     })
+// }
+// getImage('running')
+
+// function getWeather(location){
+//     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + weatherKey)
+//     .then(function(response2){
+//         console.log(response2)
+//         return response2.json(); 
+//      })
+//     .then(function(data2){
+//          console.log(data2)
         
-    })
+//          //empty html before each fetch
+//          $("#weather-container").empty();
 
-}
-getImage('running')
+//          //Current City name
+//          $("#weather-container").append("<h3>" + data2.name + "</h3>")
 
-function getWeather(location){
+//          //City weather icon
+//          var weatherCode = data.weather[0].icon
+//          var imageLink = "http://openweathermap.org/img/wn/" + weatherCode + "@2x.png"
+//          $("#weather-container").append("<img id='weatherIcon' src=" + imageLink + ">");
 
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + weatherKey)
-    .then(function(response2){
-        console.log(response2)
-        return response2.json(); 
-     })
-    .then(function(data2){
-         console.log(data2)
-        
-         //empty html before each fetch
-         $("#container").empty();
-
-         //Current City name
-         $("#container").append("<h3>" + data.name + "</h3>")
-
-         //City weather icon
-         var weatherCode = data.weather[0].icon
-         var imageLink = "http://openweathermap.org/img/wn/" + weatherCode + "@2x.png"
-         $("#container").append("<img id='weatherIcon' src=" + imageLink + ">");
-
-         //Weather data
-         $("#container").append("<p>" + data.weather[0].description + "</p><br>");
-         $("#container").append("<p>" + "Temperature: " + data.main.temp + " °F" + "</p>");
-        
-     }).catch(function(error){
-         alert("404 data not found")
-         return
-     })
+//          //Weather data
+//          $("#weather-container").append("<p>" + data.weather[0].description + "</p><br>");
+//          $("#weather-container").append("<p>" + "Temperature: " + data.main.temp + " °F" + "</p>");
+//      })
    
-}
-getWeather('Austin')
+//      .catch(function(error){
+//          alert("404 data not found")
+//          return
+//      })
+// }
+//getWeather('Austin')
+
+//submit function
+// $('#submit').on('click',function(event){
+//     event.preventDefault()
+//     //call getWeather on click
+//     getWeather(location)
+// })
