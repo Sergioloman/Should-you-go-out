65526396339bb069b106e2b74937844b95903468
Sergio Torres <sergio.lo.torres@gmail.com>
2021-03-28 19:00:20 -0500

    get weather feature completed

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,72 +1,82 @@
 //variables
 var giphyKey = 'qde5re80EUg2L5yAKth9QabSkIrGiKWb'
-var weatherKey ='324a506b2f6b0f1b44fde14916e4b006'
+var weatherKey = '324a506b2f6b0f1b44fde14916e4b006'
 
+function getImage(keyword) {
+    //get keyword value
+    /* Here we will have to create an array of posible things to do according to conditions.
+    like
 
-var location =  $("gif-container")
+    let favorable = [ running, hiking, etc ]
+      
+    let unfavorable = [ reading, painting, etc]
 
+    later we make a logic :  if CURRENT TEMP < 60 then UNFAVORABLE random keyword
+                                    or 
+                                CURRENT TEMP > 60 then FAVORABLE random keyword
+    */
+    //get a PG giphy 
+    fetch("https://api.giphy.com/v1/gifs/search?api_key=" + giphyKey + "&q=" + keyword + "&limit=25&offset=0&rating=pg&lang=en")
+        .then(function (response) {
+            console.log(response)
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data)
 
-console.log('testing javascript');
+            //empty html container
+            $('#gif-container').empty()
 
-// var keyword = $("#input").val().trim()
-// function getImage(keyword){
-//     //get keyword valie
-    
-//     //get a G giphy 
-//     fetch("https://api.giphy.com/v1/gifs/search?api_key="+ giphyKey +"&q="+ keyword +"&limit=25&offset=0&rating=g&lang=en")
-//     .then(function(response){
-//         console.log(response)
-//        return response.json(); 
-//     })
-//     .then(function(data){
-//         console.log(data)
+            //find the link for the gif's // need logic for random GIF
+            var imageUrl = data.data[0].images.original.url
+            //append image
+            $('#gif-container').append("<img id='gif' src=" + imageUrl + ">")
+        })
+}
+getImage('bike-ride')
 
-//         //empty html container
-//         $('#gif-container').empty()
-        
-//         //find the link for the gif's
-//         var imageUrl = data[0].images.original.url
-//         //append image
-//         $('#gif-container').append("<img id='gif' src=" + imageUrl + ">")
-//     })
-// }
-// getImage('running')
+function getWeather(location) {
+    fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + weatherKey)
+        .then(function (response2) {
+            console.log(response2)
+            return response2.json();
+        })
+        .then(function (data2) {
+            console.log(data2)
 
-// function getWeather(location){
-//     fetch("https://api.openweathermap.org/data/2.5/weather?q=" + location + "&units=imperial&appid=" + weatherKey)
-//     .then(function(response2){
-//         console.log(response2)
-//         return response2.json(); 
-//      })
-//     .then(function(data2){
-//          console.log(data2)
-        
-//          //empty html before each fetch
-//          $("#weather-container").empty();
+            //empty html before each fetch
+            $("#weather-container").empty();
 
-//          //Current City name
-//          $("#weather-container").append("<h3>" + data2.name + "</h3>")
+            //Current City name
+            $("#weather-container").append("<h3>" + data2.name + "</h3>")
 
-//          //City weather icon
-//          var weatherCode = data.weather[0].icon
-//          var imageLink = "http://openweathermap.org/img/wn/" + weatherCode + "@2x.png"
-//          $("#weather-container").append("<img id='weatherIcon' src=" + imageLink + ">");
+            //City weather icon
+            var weatherCode = data2.weather[0].icon
+            var imageLink = "http://openweathermap.org/img/wn/" + weatherCode + "@2x.png"
+            $("#weather-container").append("<img id='weatherIcon' src=" + imageLink + ">");
 
-//          //Weather data
-//          $("#weather-container").append("<p>" + data.weather[0].description + "</p><br>");
-//          $("#weather-container").append("<p>" + "Temperature: " + data.main.temp + " °F" + "</p>");
-//      })
-   
-//      .catch(function(error){
-//          alert("404 data not found")
-//          return
-//      })
-// }
-//getWeather('Austin')
+            //Weather data// what other conditions do we want?
+            $("#weather-container").append("<p>" + data2.weather[0].description + "</p>");
+            $("#weather-container").append("<p>" + "Temperature: " + data2.main.temp + " °F" + "</p>");
+        })
+
+        .catch(function (error) {
+            alert("404 data not found")
+            return
+        })
+}
+//Make Austin our default location
+getWeather('Austin')
 
 //submit function
-// $('#submit').on('click',function(event){
-//     event.preventDefault()
-//     //call getWeather on click
-//     getWeather(location)
-// })
+$('#submit').on("click", function (event) {
+    event.preventDefault();
+
+    //get value from form
+    var currentLocation = $("#location").val().trim();
+    console.log(currentLocation);
+
+    $("#location").val("");
+    //call getWeather on click
+    getWeather(currentLocation);
+})
