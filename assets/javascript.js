4e49b1cf209d87d2388fc9d588742979f9ad4068
Braden Welsh <BradenWelsh@users.noreply.github.com>
2021-03-30 20:13:39 -0500

    Started on Styling

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,7 +2,8 @@
 var giphyKey = 'qde5re80EUg2L5yAKth9QabSkIrGiKWb'
 var weatherKey = '324a506b2f6b0f1b44fde14916e4b006'
 
-
+var submit = document.querySelector("#submit");
+var refresh = document.querySelector(".button")
 
 function getActivity(activity){
     /*
